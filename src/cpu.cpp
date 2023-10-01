e84d5e2c49034d27ecdeb273ba4ab10b68d44e0e
Akshit Garg <garg.akshit@gmail.com>
2023-10-01 18:57:15 +0530

    feat: implement opcodes
    Signed-off-by: Akshit Garg <garg.akshit@gmail.com>

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -1,7 +1,5 @@
 #include <spdlog/sinks/stdout_color_sinks.h>
 
-#include <utility>
-
 #include "cpu.h"
 #include "utils.h"
 
@@ -218,6 +216,472 @@ void CPU::addressing_mode(op::AddressingMode mode) noexcept {
   }
 }
 
+void CPU::flag_overflow(uint16_t result, uint16_t value) noexcept {
+  // Don't ask...
+  status.V =
+      (~((uint16_t)a ^ (uint16_t)value) & ((uint16_t)a ^ (uint16_t)result)) &
+      0x80;
+}
+
+void CPU::flag_negative(uint16_t result) noexcept { status.N = result & 0x80; }
+
+void CPU::flag_zero(uint16_t result) noexcept {
+  status.Z = (result & 0xff) == 0;
+}
+
+void CPU::flag_carry(uint16_t result) noexcept { status.C = result > 0xff; }
+
+void CPU::fetch() noexcept {
+  if (decoded_opcode->mode != op::AddressingMode::Implicit)
+    fetched = read(addr_abs);
+}
+
+void CPU::execute(op::Op op) noexcept {
+  switch (op) {
+  case op::Op::ADC: {
+    fetch();
+
+    // A + fetched + carry.
+    uint16_t result = (uint16_t)a + (uint16_t)fetched + (uint16_t)status.C;
+
+    // Set the flags.
+    flag_overflow(result, fetched);
+    flag_negative(result);
+    flag_zero(result);
+    flag_carry(result);
+
+    // Put the result in the accumulator.
+    a = result & 0xff;
+
+    // TODO(AG): Check if we need an extra cycle here.
+  } break;
+
+  case op::Op::SBC: {
+    fetch();
+
+    // Bless binary. Inverting the digits makes this same as ADC.
+    uint16_t value = fetched ^ 0xff;
+
+    // A + fetched + carry.
+    uint16_t result = (uint16_t)a + (uint16_t)value + (uint16_t)status.C;
+
+    // Set the flags.
+    flag_overflow(result, value);
+    flag_negative(result);
+    flag_zero(result);
+    flag_carry(result);
+
+    // Put the result in the accumulator.
+    a = result & 0xff;
+
+    // TODO(AG): Check if we need an extra cycle here.
+  } break;
+
+  case op::Op::AND: {
+    fetch();
+    a &= fetched;
+
+    flag_zero(a);
+    flag_negative(a);
+
+    // TODO(AG): Check if we need an extra cycle here.
+  } break;
+
+  case op::Op::ASL: {
+    fetch();
+    uint16_t result = ((uint16_t)fetched) << 1;
+
+    flag_carry(result);
+    flag_negative(result);
+    flag_zero(result);
+
+    if (decoded_opcode->mode == op::AddressingMode::Implicit)
+      a = result & 0xff;
+    else
+      write(addr_abs, result & 0xff);
+  } break;
+
+  case op::Op::BCC: {
+    if (status.C == 0)
+      branch();
+  } break;
+
+  case op::Op::BCS: {
+    if (status.C != 0)
+      branch();
+  } break;
+
+  case op::Op::BEQ: {
+    if (status.Z != 0)
+      branch();
+  } break;
+
+  case op::Op::BIT: {
+    fetch();
+    uint16_t result = ((uint16_t)a) & ((uint16_t)fetched);
+
+    flag_zero(result);
+    status.N = (fetched & (1 << 7)) == 0;
+    status.V = (fetched & (1 << 6)) == 0;
+  } break;
+
+  case op::Op::BMI: {
+    if (status.N != 0)
+      branch();
+  } break;
+
+  case op::Op::BNE: {
+    if (status.Z == 0)
+      branch();
+  } break;
+
+  case op::Op::BPL: {
+    if (status.N == 0)
+      branch();
+  } break;
+
+  case op::Op::BRK: {
+    interrupt(0xfffe);
+  } break;
+
+  case op::Op::BVC: {
+    if (status.V == 0)
+      branch();
+  } break;
+
+  case op::Op::BVS: {
+    if (status.V != 0)
+      branch();
+  } break;
+
+  case op::Op::CLC: {
+    status.C = 0;
+  } break;
+
+  case op::Op::CLD: {
+    status.D = 0;
+  } break;
+
+  case op::Op::CLI: {
+    status.I = 0;
+  } break;
+
+  case op::Op::CLV: {
+    status.V = 0;
+  } break;
+
+  case op::Op::CMP: {
+    fetch();
+
+    uint16_t result = (uint16_t)a - (uint16_t)fetched;
+
+    status.C = a >= fetched;
+    flag_zero(result);
+    flag_negative(result);
+  } break;
+
+  case op::Op::CPX: {
+    fetch();
+
+    uint16_t result = (uint16_t)x - (uint16_t)fetched;
+
+    status.C = x >= fetched;
+    flag_zero(result);
+    flag_negative(result);
+  } break;
+
+  case op::Op::CPY: {
+    fetch();
+
+    uint16_t result = (uint16_t)y - (uint16_t)fetched;
+
+    status.C = y >= fetched;
+    flag_zero(result);
+    flag_negative(result);
+  } break;
+
+  case op::Op::DEC: {
+    fetch();
+
+    uint16_t result = fetched - 1;
+    write(addr_abs, result & 0xff);
+
+    flag_zero(result);
+    flag_negative(result);
+  } break;
+
+  case op::Op::DEX: {
+    x--;
+    flag_zero(x);
+    flag_negative(x);
+  } break;
+
+  case op::Op::DEY: {
+    y--;
+    flag_zero(y);
+    flag_negative(y);
+  } break;
+
+  case op::Op::EOR: {
+    fetch();
+    a ^= fetched;
+
+    flag_zero(a);
+    flag_negative(a);
+
+    // TODO(AG): Check for cycle penalty here.
+  } break;
+
+  case op::Op::INC: {
+    fetch();
+
+    uint16_t result = fetched + 1;
+    write(addr_abs, result & 0xff);
+
+    flag_zero(result);
+    flag_negative(result);
+  } break;
+
+  case op::Op::INX: {
+    x++;
+    flag_zero(x);
+    flag_negative(x);
+  } break;
+
+  case op::Op::INY: {
+    y++;
+    flag_zero(y);
+    flag_negative(y);
+  } break;
+
+  case op::Op::JMP: {
+    pc = addr_abs;
+  } break;
+
+  case op::Op::JSR: {
+    // We push the current PC due to how 6502 works.
+    pc--;
+    push_pc();
+    pc = addr_abs;
+  } break;
+
+  case op::Op::LDA: {
+    fetch();
+    a = fetched;
+    flag_zero(a);
+    flag_negative(a);
+  } break;
+
+  case op::Op::LDX: {
+    fetch();
+    x = fetched;
+    flag_zero(x);
+    flag_negative(x);
+  } break;
+
+  case op::Op::LDY: {
+    fetch();
+    y = fetched;
+    flag_zero(y);
+    flag_negative(y);
+  } break;
+
+  case op::Op::NOP: {
+    // TODO(AG): Add aliased NOPs (unknown instructions).
+  } break;
+
+  case op::Op::ORA: {
+    fetch();
+    a |= fetched;
+    flag_zero(a);
+    flag_negative(a);
+  } break;
+
+  case op::Op::PHA: {
+    push(a);
+  } break;
+
+  case op::Op::PHP: {
+    uint8_t old_flags = p;
+
+    status._ = 1;
+    status.B = 1;
+    push(p);
+
+    p = old_flags;
+  } break;
+
+  case op::Op::PLA: {
+    a = pop();
+    flag_zero(a);
+    flag_negative(a);
+  } break;
+
+  case op::Op::PLP: {
+    p = pop();
+    status._ = 1;
+  } break;
+
+  case op::Op::ROL: {
+    fetch();
+    uint16_t result = (((uint16_t)fetched) << 1) | status.C;
+
+    flag_zero(result);
+    flag_negative(result);
+    flag_carry(result);
+
+    if (decoded_opcode->mode == op::AddressingMode::Implicit)
+      a = result & 0xff;
+    else
+      write(addr_abs, result & 0xff);
+  } break;
+
+  case op::Op::ROR: {
+    fetch();
+    uint16_t result = (((uint16_t)fetched) >> 1) | (((uint16_t)status.C) << 7);
+
+    flag_zero(result);
+    flag_negative(result);
+    flag_carry(result);
+
+    if (decoded_opcode->mode == op::AddressingMode::Implicit)
+      a = result & 0xff;
+    else
+      write(addr_abs, result & 0xff);
+  } break;
+
+  case op::Op::RTI: {
+    p = pop();
+    status._ = 1;
+    status.B = 1;
+    pop_pc();
+  } break;
+
+  case op::Op::RTS: {
+    pop_pc();
+    pc++;
+  } break;
+
+  case op::Op::SEC: {
+    status.C = 1;
+  } break;
+
+  case op::Op::SED: {
+    status.D = 1;
+  } break;
+
+  case op::Op::SEI: {
+    status.I = 1;
+  } break;
+
+  case op::Op::STA: {
+    write(addr_abs, a);
+  } break;
+
+  case op::Op::STX: {
+    write(addr_abs, x);
+  } break;
+
+  case op::Op::STY: {
+    write(addr_abs, y);
+  } break;
+
+  case op::Op::TAX: {
+    x = a;
+    flag_zero(x);
+    flag_negative(x);
+  } break;
+
+  case op::Op::TAY: {
+    y = a;
+    flag_zero(y);
+    flag_negative(y);
+  } break;
+
+  case op::Op::TSX: {
+    x = sp;
+    flag_zero(x);
+    flag_negative(x);
+  } break;
+
+  case op::Op::TXA: {
+    a = x;
+    flag_zero(a);
+    flag_negative(a);
+  } break;
+
+  case op::Op::TXS: {
+    sp = x;
+  } break;
+
+  case op::Op::TYA: {
+    a = y;
+    flag_zero(a);
+    flag_negative(a);
+  } break;
+
+  default: {
+    PANIC("Unimplemented instruction");
+  } break;
+  }
+}
+
+void CPU::branch() noexcept { // Branch!
+  pending_cycles++;
+
+  // Branches are relative.
+  addr_abs = pc + addr_rel;
+
+  // Check if we have a page cross penalty.
+  if ((addr_abs & 0xff00) != (pc & 0xff00))
+    pending_cycles++;
+
+  pc = addr_abs;
+}
+
+void CPU::interrupt(uint16_t vector) noexcept {
+  logger->trace("Performing CPU interrupt with the vector {:#06x}", vector);
+
+  status.I = 1;
+  push_pc();
+
+  // NOTE(AG): Decimal mode shenanigans on the NES 6502.
+  //           Should change this while porting this CPU to other platforms.
+  status.B = 1;
+  push(p);
+  status.B = 0;
+
+  pc = read16(vector);
+  dump_reg();
+}
+
+void CPU::push(uint8_t value) noexcept {
+  logger->trace("Pushing {:#04x} on stack pos {:#04x}", value, sp);
+
+  write(0x0100 + sp, value);
+  sp--;
+}
+
+void CPU::push_pc() noexcept {
+  push((pc >> 8) & 0xff);
+  push(pc & 0xff);
+}
+
+uint8_t CPU::pop() noexcept {
+  sp++;
+  uint8_t value = read(0x0100 + sp);
+
+  logger->trace("Popped {:#04x} from stack pos {:#04x}", value, sp);
+  return value;
+}
+
+void CPU::pop_pc() noexcept {
+  auto lo = pop();
+  auto hi = pop();
+
+  pc = ((uint16_t)lo) | (((uint16_t)hi) << 8);
+}
+
 void CPU::tick() noexcept {
   logger->trace("Tick.");
 
@@ -233,17 +697,21 @@ void CPU::tick() noexcept {
   opcode = read(pc);
   pc++;
 
-  auto op = op::opcodes[opcode];
-  logger->trace("Executing opcode {}", op);
+  decoded_opcode = &op::opcodes[opcode];
+  logger->trace("Executing opcode {}", *decoded_opcode);
 
-  if (op.unknown) {
+  if (decoded_opcode->unknown) {
     PANIC("Unknown opcode.");
   }
 
   // Set the pending cycles.
-  pending_cycles = op.cycles;
+  pending_cycles = decoded_opcode->cycles;
   // Calculate stuff using the addressing mode.
-  addressing_mode(op.mode);
+  addressing_mode(decoded_opcode->mode);
+  // Perform the actual instruction.
+  execute(decoded_opcode->operation);
+
+  status._ = 1;
 
   // Perform a sanity check.
   sanity();
--- src/cpu.h
+++ src/cpu.h
@@ -65,8 +65,36 @@ class CPU : private Registers {
   uint16_t addr_rel = 0;
   // Opcode that is being currently executed.
   uint8_t opcode = 0;
+  const op::Opcode* decoded_opcode;
 
+  // Compute addresses and stuff using the addressing mode.
   void addressing_mode(op::AddressingMode mode) noexcept;
+  // Execute the actual instruction.
+  void execute(op::Op op) noexcept;
+
+  // Compute the overflow for an arithmetic operation. Must be called before
+  // setting the accumulator.
+  void flag_overflow(uint16_t result, uint16_t value) noexcept;
+  // Compute the negative flag.
+  void flag_negative(uint16_t result) noexcept;
+  // Compute the zero flag.
+  void flag_zero(uint16_t result) noexcept;
+  // Compute the carry flag.
+  void flag_carry(uint16_t result) noexcept;
+
+  // Fetch the value from the addr_abs.
+  void fetch() noexcept;
+
+  // Branch using addr_rel.
+  void branch() noexcept;
+
+  // Performs an interrupt using the provided interrupt vector.
+  void interrupt(uint16_t vector) noexcept;
+
+  void push(uint8_t value) noexcept;
+  void push_pc() noexcept;
+  uint8_t pop() noexcept;
+  void pop_pc() noexcept;
 
 public:
   CPU(ReadFunction read, WriteFunction write) noexcept;
