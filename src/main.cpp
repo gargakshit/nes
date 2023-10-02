4be61b5fafd681408f2a6d9c0ca704c5d28a5597
Akshit Garg <garg.akshit@gmail.com>
2023-10-02 09:23:54 +0530

    refactor: reduce opcode table size
    Signed-off-by: Akshit Garg <garg.akshit@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ void setup_spdlog() {
   auto logger = spdlog::stderr_color_mt("nes");
   spdlog::set_default_logger(logger);
 
-  spdlog::set_level(spdlog::level::trace);
+  spdlog::set_level(spdlog::level::info);
 }
 
 int main([[maybe_unused]] const int argc, [[maybe_unused]] const char **argv) {
